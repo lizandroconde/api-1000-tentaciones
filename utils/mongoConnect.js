16f6f7562f3e3e3fd3d05de1ef8e73dda579370a
Lizandro <lizandroconde@gmail.com>
2020-12-30 18:42:07 -0500

    actualizacion de la url de api

--- utils/mongoConnect.js
+++ utils/mongoConnect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const mongoConnect = () => new Promise((resolve, reject) => {
-    mongoose.connect('mongodb+srv://lizandroconde:lizandroconde@cluster0.obhah.mongodb.net/1000tentaciones', { useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify:true }).then(() => {
+    mongoose.connect('mongodb+srv://lizandroconde:lizandroconde@cluster0.obhah.mongodb.net/1000tentaciones?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify:true }).then(() => {
         resolve('MongoDB is working');
     }).catch(err => {
         reject(err);
